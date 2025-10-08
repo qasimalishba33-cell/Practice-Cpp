af7ef9d01d9d74a0eaeb9b3c08f6fbe4ce147386
Alishba Qasim <qasimalishba33@gmail.com>
2025-10-08 16:29:44 +0500

    ternary

---
+++ ternary.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int x,y,z;
+    cout<<"Enter first no:";
+    cin>>x;
+    cout<<"Enter second no:";
+    cin>>y;
+    cout<<"Enter third no:";
+    cin>>z;
+    int largest= x>y?((x>z)?x:z):((y>z)?y:z);
+    
+        cout<<largest;
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+}
