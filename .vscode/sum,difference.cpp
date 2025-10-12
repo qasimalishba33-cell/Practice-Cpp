46bc9365a2ab486c0cec909eec571facb240b980
Alishba Qasim <qasimalishba33@gmail.com>
2025-10-12 09:03:50 +0500

    sumdifference

---
+++ .vscode/sum,difference.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int a,b,sum,difference,product,quotient,rem;
+    cout<<"enter the num1:";
+    cin>>a;
+    cout<<"Enter the num2:";
+    cin>>b;
+    sum = a+b;
+    cout<<"sum of a and b is:"<<sum<<endl;
+    difference = a-b;
+    cout<<"difference ofa and b is:"<<difference<<endl;
+    
+    product = a*b;
+    cout<<"product of a and b is:"<<product<<endl;
+    quotient = a/b;
+    cout<<"quotient of a and b is:"<<quotient<<endl;
+    rem = a%b;
+    cout<<"rem of a and b is:"<<rem<<endl;
+}
+    
