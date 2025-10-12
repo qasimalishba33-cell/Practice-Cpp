d0ae858990d66b39efa4940da069954c9ac8edce
Alishba Qasim <qasimalishba33@gmail.com>
2025-10-12 08:47:44 +0500

    AreaPerimeter

---
+++ .vscode/AreaPerimeter.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int area,l,w,perimeter;
+    cout<<"Enter lengh of rectangle:";
+    cin>>l;
+    cout<<"Enter width of rectangle:";
+    cin>>w;
+    area = l*w;
+    
+    
+    cout<<"your area is";
+cout<<area<<endl;
+    perimeter = l*l + w*w;
+    
+    cout<<"your perimeter is";
+
+    cout<<perimeter;
+}
