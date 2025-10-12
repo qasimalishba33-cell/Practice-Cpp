1158d89e85bea86225c6a1ace9d50d4758e3bcc0
Alishba Qasim <qasimalishba33@gmail.com>
2025-10-12 08:34:53 +0500

    NameAge

---
+++ .vscode/NameAge.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    string name;
+    int age;
+    cout<<"Enter your name:";
+    cin>>name;
+    cout<<"your age is:";
+    cin>>age;
+    cout<<"Hello,Alishba your age is 18 years old";
+}
